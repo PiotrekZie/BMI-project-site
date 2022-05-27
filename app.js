c8b4f54e64143d6fd04d312f3efca34a928a2102
Pete <zzieluuuu@gmail.com>
2022-05-27 15:56:51 +0200

    active class on btns, css fixes

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+function sel() {
+// Get the container element
+var btnContainer = document.getElementById("gender");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+
+    // If there's no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    // Add the active class to the current/clicked button
+    this.className += " active";
+  });
+} 
+}
+
 function next() {
 
     var main=document.getElementById('one');
@@ -9,12 +32,12 @@ function next() {
 
 function back() {
 
-        var main=document.getElementById('one');
-        main.style.display='flex';
+    var main=document.getElementById('one');
+    main.style.display='flex';
     
-        var calculator=document.getElementById('two');
-        calculator.style.display='none';
-        }
+    var calculator=document.getElementById('two');
+    calculator.style.display='none';
+    }
 
 window.onload = () => {
     let button = document.querySelector("#submit")
@@ -82,3 +105,4 @@ function calculatorBMI() {
             `Your BMI</br> Over Weight : <span>${bmi}</span>`;
     }
 }
+
